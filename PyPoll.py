9a57d60ce86c9bb831bcf61f040b22e2ece51799
Betsy-Kalkwarf <109998791+Betsy-Kalkwarf@users.noreply.github.com>
2022-08-25 06:45:30 -0700

    Add files via upload

---
+++ PyPoll.py
@@ -0,0 +1,42 @@
+
+#The data we need to retrieve
+import csv
+
+import os
+
+# Assign a variable to load a file from a path.
+file_to_load = '/Users/Betsy/Desktop/UCSD Bootcamp/Unit 2 Python/election_analysis/Resources/election_results.csv'
+
+
+# Assign a variable to save the file to a path.
+file_to_save = '/Users/Betsy/Desktop/UCSD Bootcamp/Unit 2 Python/election_analysis/Resources/election_analysis.txt'
+  
+
+#Open the election results and read the file
+with open(file_to_load) as election_data:
+
+
+#To Do: read and analyze data here
+
+    # Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+
+    # Print the header row.
+    headers = next(file_reader)
+    print(headers)
+    ## Print each row in the CSV file.
+    #for row in file_reader:
+     #   print(row)
+#1. The total number of votes cast
+
+#2. A complete list of candidates who received votes
+
+#3. The percentage of votes each candidae won
+
+#4. The total number of votes each candidate won
+
+#5. The winner of the election based on popular vote
+
+
+
+
